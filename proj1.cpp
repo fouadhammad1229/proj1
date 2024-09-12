73912bf8fa6cb1645d9e051c574234a02af38fda
T0m0ko <thelolbox101@gmail.com>
2024-09-12 10:56:45 -0400

    Second version: Added Letter counter. Poor formatting

--- proj1.cpp
+++ proj1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int counterWords(string str)
 {
-		// When the string is empty or null, return to 0.
+	// When the string is empty or null, return to 0.
 	if (str.size() == 0) {
 		return 0;
 	}
@@ -24,11 +24,27 @@ int counterWords(string str)
 		}
 	}
 
+
 	int counter = 1;
 
 	for (int i = 0; i < words.size(); i++) {
 		if (words[i].size() != 0)
 			counter++;
+	}
+
+
+	// Count the amount of letters in a text
+	
+	int letters[26]{0};
+
+	for (int i = 0; i < str.length(); i++) {
+		letters[toupper(str[i]) - 65] += 1; // Add 1 to element holding a letter
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if(letters[i] > 0) {
+			cout << char(65 + i) << ": Showed Up: " << letters[i] << endl;
+		}
 	}
 
 	return counter;
@@ -44,8 +60,7 @@ int main()
 
 	getline(cin, str);
 
-	cout << "No of words: " << counterWords(str) << endl;
+	cout << counterWords(str) << ": words in total." << endl;
 
 	return 0;
 }
-
